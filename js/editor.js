94d989dd6daba5e1639e724ddf098c9997aa1254
Evgeniy Solovey <e.solovey@yandex.ru>
2017-04-11 11:54:32 +0200

    Add js for WP visual editor

---
+++ js/editor.js
@@ -0,0 +1,33 @@
+(function() {
+    tinymce.create("tinymce.plugins.swi_button_plugin", {
+        init : function(ed, url) {
+            ed.addButton("swinfo", {
+                title : "Add software shortcode",
+                cmd : "swinfo_add",
+                image : "https://cdn4.iconfinder.com/data/icons/linecon/512/add-32.png"
+            });
+
+            //button functionality.
+            ed.addCommand("swinfo_add", function() {
+                var selected_text = ed.selection.getContent();
+                var return_text = "[swinfo id='' template='modern']";
+                ed.execCommand("mceInsertContent", 0, return_text);
+            });
+
+        },
+
+        createControl : function(n, cm) {
+            return null;
+        },
+
+        getInfo : function() {
+            return {
+                longname : "Extra Buttons",
+                author : "Evgenii Solovei",
+                version : "1.0.1"
+            };
+        }
+    });
+
+    tinymce.PluginManager.add("swi_button_plugin", tinymce.plugins.swi_button_plugin);
+})();
